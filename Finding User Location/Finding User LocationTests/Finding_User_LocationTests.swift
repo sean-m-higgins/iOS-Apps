6c2d118a071c39343a30eac315932a960bd56528
Sean Higgins <shiggins@luc.edu>
2019-06-20 22:05:15 -0500

    added user location app

---
+++ Finding User Location/Finding User LocationTests/Finding_User_LocationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Finding_User_LocationTests.swift
+//  Finding User LocationTests
+//
+//  Created by Sean Higgins on 6/20/19.
+//  Copyright © 2019 Sean Higgins. All rights reserved.
+//
+
+import XCTest
+@testable import Finding_User_Location
+
+class Finding_User_LocationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
