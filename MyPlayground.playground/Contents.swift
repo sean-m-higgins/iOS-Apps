417eaf5247d7c76b8fecfdf7d2ef2faa4e1697d8
Sean Higgins <shiggins@luc.edu>
2019-06-20 19:51:13 -0500

    added playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print("s")
+
+let other = "hello"
+
+var my_int = 20
+
+print("my_int value: \(my_int)")
+
+var array = [30, 50, 60, 70]
+
+print(array[3])
+
+array.append(90)
+
+array.remove(at: 3)
+
+let new_arr: Any = ["one", 1, true]
+
+var dict = ["computer": 1, "coffee": 2]
+
+print(dict["computer"]!)
+
+dict["pen"] = 3
+
+print(dict)
+
+var new_dict = [String: Double]()
+
+let dice_roll = arc4random_uniform(6) + 1
+
+var num: Int?
+
+
+
